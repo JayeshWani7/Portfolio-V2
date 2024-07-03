5421ccc16812cfb922ee65106080c82bc081be21
Jayesh Wani <106749064+JayeshWani7@users.noreply.github.com>
2024-07-03 23:59:21 +0530

    projects updations

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -1,47 +1,20 @@
 export const Work = [
     {
         id:1,
-        name:"Cinefacts",
-        description:"It is build on top of the React JS, and the Data is Coming from TMDB(The Movie DataBase).",
-        tags:["react js","TMDB","redux"],
+        name:"DSABUDDY",
+        description:"A React-based web application that serves as a DSA visualizer and problem-solving assistant, enhancing learning and understanding of data structures and algorithms.",
+        tags:["react","DSA","visualization"],
         
-        demo:"https://cine-facts.vercel.app/",
-        github:"https://github.com/Kunal0612/CineFacts"
+        demo:"https://dsabuddy-eta.vercel.app/",
+        github:"https://github.com/JayeshWani7/DSABUDDY"
     },
     {
         id:2,
-        name:"DOCTOR APPOINTMENT APP : MERN STACK PROJECT",
-        description:"A Doctor appointment booking web app where patient can book doctors for their checkups.",
-        tags:["react","gsap","styledComponents"],
+        name:"Electronics Gadgets Website",
+        description:"A website with an admin panel and gadgets repairing features, built using PHP and MySQL for efficient management and customer service.",
+        tags:["PHP","MySQL","admin panel"],
         
-        demo:"https://medci-frontend.vercel.app/",
-        github:"https://github.com/Kunal0612/Hacktivist_MEDCI"
-    },
-    {
-        id:3,
-        name:"Snake Game",
-        description:"It is build using HTML , CSS and Javascript with DOM functionality",
-        tags:["Valina Js","HTML","CSS"],
-        
-        demo:"https://snake-game-xi-sooty.vercel.app/",
-        github:"https://github.com/Kunal0612/Snake-Game"
-    },
-    {
-        id:4,
-        name:"Write Here (To Do List)",
-        description:"Developed a functionality to store notes for individuals to securely store and retrieve the notes on the user’s device.",
-        tags:["HTML","CSS","JAVASCRIPT"],
-        
-        demo:"https://replit.com/@KunalShaw3/Writehere",
-        github:"https://replit.com/@KunalShaw3/Writehere"
-    },
-    {
-        id:5,
-        name:"SIDCUP FAMILY GOLF : GSAP-ANIMATED WEBSITE",
-        description:"Developed a functionality to store notes for individuals to securely store and retrieve the notes on the user’s device.",
-        tags:["HTML","CSS","JAVASCRIPT"],
-        
-        demo:"https://sidcup-family-golf-gsap-animated-website.vercel.app/",
-        github:"https://github.com/Kunal0612/SIDCUP-FAMILY-GOLF-GSAP-ANIMATED-WEBSITE"
-    },
+        demo:"",  // Add your demo link if available
+        github:"https://github.com/JayeshWani7/Electronic-Gadgets"
+    }
 ] 
