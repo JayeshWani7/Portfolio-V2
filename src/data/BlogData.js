2e51c9b0a0a683b4cd67da28328b7b4073aa9815
Jayesh Wani <106749064+JayeshWani7@users.noreply.github.com>
2024-07-20 01:17:47 +0530

    coding profiles changes

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -23,14 +23,14 @@ export const Blogs = [
     {
         id:4,
         name:"Codechef",
-        tags:["2 ★","1550 Max","Top 20k Globally", "Ranked 322 in Starters143"],
+        tags:["2★","1550 Max","Top 20k Globally", "Ranked 322 in Starters143"],
         imgSrc:"https://images.yourstory.com/cs/images/companies/codechef-1592821643695.jpg?fm=auto&ar=1:1&mode=fill&fill=solid&fill-color=fff",
         link:"https://www.codechef.com/users/jayeshwani"
     },
     {
         id:5,
         name:"Atcoder",
-        tags:[],
+        tags:["Brown", "166"],
         imgSrc:"https://i.ytimg.com/vi/2kGhxVdSlTQ/mqdefault.jpg",
         link:"https://atcoder.jp/users/Jayeshwani7"
     },
